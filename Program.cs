edb6ee27dd57b4b62e4026b45b6e84cc99710796
Bishal <bishalshah431@gmail.com>
2024-11-19 17:55:06 +0545

    Updated UI

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
+        pattern: "{controller=Todo}/{action=GetTodos}/{id?}")
     .WithStaticAssets();
 
 
